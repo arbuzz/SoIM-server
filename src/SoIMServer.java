1f4c8e8342f5e5bf91f04e069a89e28c0e04c6cd
Konstantin Olshanikov <k.olshanikov@akzia.ru>
2011-09-15 18:15:56 +0400

    Немного доделан каркас, забыл пару штук сделать.

--- src/SoIMServer.java
+++ src/SoIMServer.java
@@ -1,6 +1,10 @@
+import codec.XMPPCodecFactory;
 import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.net.InetSocketAddress;
 
 /**
  * This code is brought you by
@@ -9,9 +13,12 @@ import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
  */
 public class SoIMServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         IoAcceptor acceptor = new NioSocketAcceptor();
 
         acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+        acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new XMPPCodecFactory()));
+        acceptor.setHandler(new SoIMHandler());
+        acceptor.bind(new InetSocketAddress(5222));
     }
 }
